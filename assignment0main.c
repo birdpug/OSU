01ffa6e4dead698177a91627d403cc1d24b79299
birdpug <49289582+birdpug@users.noreply.github.com>
2019-04-06 11:53:52 -0500

    Update assignment0main.c

--- assignment0main.c
+++ assignment0main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#pragma warning(disable : 4996)
 
 int main()
 {
@@ -9,13 +9,13 @@ int main()
 	int dog=0;
 	int mouse=0;
 	int rat=0;	
-	scanf_s("%d", &dog);
+	scanf("%d", &dog);
 	printf("Ok, Please enter a number that would be the length of the next side of your triangle.\n");
-	scanf_s("%d", &mouse);
+	scanf("%d", &mouse);
 	printf("Ok, Please enter a number that would be the length of the next side of your triangle.\n");
-	scanf_s("%d", &rat);
+	scanf("%d", &rat);
 
-	int ferret = dog + mouse + rat;
+	double ferret = dog + mouse + rat;
 	ferret = ferret / 2;
 	dog = ferret - dog;
 	mouse = ferret - mouse;
@@ -25,7 +25,7 @@ int main()
 	printf("Ok, the area is ");
 
 	printf("%d", ferret);
-	scanf_s("%d", &rat);
+	scanf("%d", &rat);
 
 	return 0;
 }
